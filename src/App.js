aa26808facae5fad7c9b42cd16f7af009b35bf91
Matt Evans <matthewevans0911@gmail.com>
2023-12-16 22:16:37 -0500

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import MiniDrawer from "./navigation/Drawer";
+// import HomePage from "./pages/HomePage";
+// import Math from "./pages/Math";
+// import Emotions from "./pages/Emotions";
+// import Reading from "./pages/Reading";
+// import Technology from "./pages/Technology";
+import "../src/styles/main.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MiniDrawer />
+      <h1>Test</h1>
     </div>
   );
 }
-
 export default App;
---
+++ src/pages/Emotions.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Emotions() {
+  return <div>Emotions</div>;
+}
